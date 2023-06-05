a10db019ca1e72011485dac37f747eb8e3895ced
Muhammad Fadli Fathurrahman <fadlifathurrahmanm@gmail.com>
2023-06-05 21:53:17 +0700

    tugas

---
+++ tugas-react-3/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  const fullName = "Muhammad Fadli Fathurrahman";
+  return (
+    <div className="header">
+      <h1>{fullName}</h1>
+    </div>
+  );
+}
+
+export default Header;
